0b4db94106f6ba30615f5220ac7e4c7880013ac0
Tetiana Antonenko <tetiana.antonenko@tui.co.uk>
2023-07-25 18:31:37 +0300

    Refactoring proposal: 1. variables named in english 2. no comments, if you need comments - than it is too complex to understand, so you need to refactor to something simple comments required for something specific and rare comments could lead you for further misunderstanding if they are no maintain (eg logic inside method changed but comment was not changed, next person who will see method - will get nuts trying to understand) 3. common logic should be extracted to methods, and then you can just re-use it 4. take a look into 55 line - i left a comment for you here

--- 5.6.1 (HW-02).py
+++ 5.6.1 (HW-02).py
@@ -1,34 +1,33 @@
-pobeditel = 0 # Это переменная для уточнения какой игров победил и должны ходи продолжиться
-varianti = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] # это список цифр из которого формируеться карта
-igrok_1 = [] # список ходов игрока 1
-igrok_2 = [] # список ходов игрока 2
-summa_push = 0 # количество ходов
-win_nummbers = [['1', '2', '3'], # выигрышные комбинации, программа постоянно проверет, есть ли комбинация в списке игрока
-                ['4', '5', '6'],
-                ['7', '8', '9'],
-                ['1', '4', '7'],
-                ['2', '5', '8'],
-                ['3', '6', '9'],
-                ['1', '5', '9'],
-                ['3', '5', '7']
-                ]
+winner_id = 0
+game_map = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+player_1_moves = []
+player_2_moves = []
+count_of_moves = 0
+max_num_moves = 9
+winning_numbers = [['1', '2', '3'],
+                   ['4', '5', '6'],
+                   ['7', '8', '9'],
+                   ['1', '4', '7'],
+                   ['2', '5', '8'],
+                   ['3', '6', '9'],
+                   ['1', '5', '9'],
+                   ['3', '5', '7']]
 
-def tabliza(x):
-    """Эта функция выводит карту ходов и карту с ходами"""
+
+def print_game_map(x):
     print(f"{x[6]} {x[7]} {x[8]}\n"
           f"{x[3]} {x[4]} {x[5]}\n"
           f"{x[0]} {x[1]} {x[2]}")
 
 
-def fahler(x):
-    """Эта функция проверяет ошибки ввода и на свободное место для хода"""
+def handle_player_actions(x):
     while True:
         try:
             choice = int(input(f"{x} игрок. Выберите место (цифру): ")) - 1
             if choice < 0 or choice > 9:
                 print("Не правильная цифра")
                 continue
-            elif varianti[choice] == 'o' or varianti[choice] == 'x':
+            elif game_map[choice] == 'o' or game_map[choice] == 'x':
                 print("Место уже занято")
                 continue
             else:
@@ -38,9 +37,8 @@ def fahler(x):
             continue
 
 
-def summa(win_nummbers, choice):
-    """Это функция проверяет список ходов игрока на наличие выигрышной комбинации"""
-    for item in win_nummbers:
+def check_winning_combination(choice):
+    for item in winning_numbers:
         count = 0
         for item_2 in item:
             if item_2 in choice:
@@ -51,10 +49,20 @@ def summa(win_nummbers, choice):
                     continue
             count = 0
 
+def update_game_board(player_moves, choice, move):
+    game_map.pop(choice)
+    game_map.insert(choice, move)
+    print_game_map(game_map)
+    player_moves.append(str(choice + 1))
+
+def print_winner(winner_id):
+    print()
+    print()
+    print(f"Итог такой - {winner_id} игрок выиграл!!! Поздравляем")
 
 while True:
-    """Этот цикл запускаеться первым и спрашивает пользователя продолжить программу или нет"""
-    start = input("""Играем? "Да" или "Нет": """).upper()
+    start = input(
+        """Играем? "Да" или "Нет": """).upper()  ##### тут ты спрашиваешь Да или Нет но ожидаешь ДА или НЕТ
     if start != "ДА" and start != 'НЕТ':
         print('Ошиблись с вводом. Попробуйте ответить ещё раз')
         continue
@@ -62,42 +70,31 @@ while True:
         print('Очень жаль =(')
         break
     else:
-        print('Отлично!!! Первый игрок ходит ноликами, а Второй крестиками. Первый ход за ноликами\nПоехали!!!')
+        print(
+            'Отлично!!! Первый игрок ходит ноликами, а второй крестиками. Первый ход за ноликами\nПоехали!!!')
         print()
-        tabliza(varianti)
+        print_game_map(game_map)
         break
 
-while not pobeditel and start == 'ДА' and summa_push != 9:
-    """Это цикл хода первого игрока"""
+
+while not winner_id and start == 'ДА' and count_of_moves != max_num_moves:
     print()
-    choice_1 = fahler('Первый')
-    varianti.pop(choice_1)
-    varianti.insert(choice_1, 'o')
-    tabliza(varianti)
-    igrok_1.append(str(choice_1 + 1))
-    summa_push += 1
-    if summa(win_nummbers, igrok_1):
-        pobeditel = "Первый игрок выиграл!!!"
-        print()
-        print()
-        print(f"Итог такой - {pobeditel} Поздравляем")
+    choice_1 = handle_player_actions('Первый')
+    update_game_board(player_1_moves, choice_1, 'o')
+    count_of_moves += 1
+    if check_winning_combination(player_1_moves):
+        print_winner('Первый')
         break
-    while summa_push != 9:
+    while count_of_moves != 9:
         """Это цикл хода второго игрока"""
         print()
-        choice_2 = fahler("Второй")
-        varianti.pop(choice_2)
-        varianti.insert(choice_2, 'x')
-        tabliza(varianti)
-        igrok_2.append(str(choice_2 + 1))
-        summa_push += 1
-        if summa(win_nummbers, igrok_2):
-            pobeditel = "Второй игрок выиграл!!!"
-            print()
-            print()
-            print(f"Итог такой - {pobeditel} Поздравляем")
-            break
+        choice_2 = handle_player_actions("Второй")
+        update_game_board(player_2_moves, choice_2, 'x')
+        count_of_moves += 1
+        if check_winning_combination(player_2_moves):
+            print_winner('Второй')
         break
 print()
-if summa_push == 9:
+if count_of_moves == 9:
     print("Победителя нет. Ничья")
+
